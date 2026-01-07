3967f8c2a3b84c5934e1ef9d21583b81f5fe65c2
nnyouung <211_0@kookmin.ac.kr>
2026-01-07 11:38:57 +0900

    feat: app.js 구현

---
+++ public/app.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("chatInput");
+const sendButton = document.getElementById("sendButton");
+
+// 디바운스 로직
+let timerId;
+function debounce(callback, delay) {
+  if (timerId) clearTimeout(timerId);
+  timerId = setTimeout(callback, delay);
+}
+
+sendButton.addEventListener("click", async () => {
+  const message = input.value;
+  debounceTranslate(async () => {
+    if (!message.trim()) return;
+
+    const clovaResponse = await fetch("/chat", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message }),
+    });
+
+    const data = await clovaResponse.json();
+
+    input.value = "";
+    console.log(data.message);
+    // TODO: data.message 채팅 버블에 띄우기
+  }, 1000);
+});
